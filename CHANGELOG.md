ec02f5640ac0bd580f63d9d164088b8cb5e17d94
Adriano Paz <contato@adrianopaz.com>
2025-03-19 17:40:23 +0900

    Inserted Header Constant Strings

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,4 @@ First commit
 # 1.0.2 - Update README
 # 1.0.3 - Fixed path Samples into package.json and informations into Changelog
 # 1.0.4 - Fixed version into package.json
+# 1.0.5 - Inserted Header Constant Strings
---
+++ Runtime/Scripts/Core/Utility/Header.cs
@@ -0,0 +1,37 @@
+namespace ASPax.Utilities
+{
+    public static class Header
+    {
+        public const string MANAGEABLE = "MANAGEABLE";
+        public const string READONLY = "READ ONLY";
+        public const string UI = "USER INTERFACE";
+        public const string ANIMATIONS = "ANIMATIONS";
+        public const string components = "COMPONENTS";
+        public const string variables = "VARIABLES";
+        public const string arrays = "ARRAYS";
+        public const string lists = "LISTS";
+        public const string scripts = "SCRIPTS";
+        public const string prefabs = "PREFABRICATEDS";
+        public const string assets = "ASSETS";
+        public const string booleans = "..Booleans..";
+        public const string integers = "..Integers..";
+        public const string floaters = "..Floaters..";
+        public const string images = "..Images..";
+        public const string sprites = "..Sprites..";
+        public const string buttons = "..Buttons..";
+        public const string animators = "..Animator Controllers..";
+        public const string animationClips = "..Animation Clips..";
+        public const string audioSources = "..Audio Sources..";
+        public const string audioClips = "..Audio Clips..";
+        public const string gameObjcts = "..Game Objects..";
+        public const string TMPro = "..Texts Mesh Pro..";
+        public const string texts = "..Texts (Legacy)..";
+        public const string transforms = "..Transforms..";
+        public const string enums = "..Enumerations..";
+        public const string colors = "..Colors..";
+        public const string vectors = "..Vectors..";
+        public const string linesRenderer = "..Lines Renderer..";
+        public const string strings = "..Strings..";
+        public const string assetMessage = "Assets are automatically assigned. If there is no assignment, use reset in script options.";
+    }
+}
